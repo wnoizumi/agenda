e90e8c979a6ddb981727abf37ddd7d9b67eb3987
Francielxyz <ruanvha15@gmail.com>
2021-10-19 20:56:31 -0300

    Alterado geração de IDS das entidades

--- src/main/java/br/ifpr/agenda/dominio/Contato.java
+++ src/main/java/br/ifpr/agenda/dominio/Contato.java
@@ -26,7 +26,7 @@ import org.springframework.format.annotation.DateTimeFormat;
 public class Contato implements Serializable {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@NotBlank(message = "Nome obrigatório")
